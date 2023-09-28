93ab9737c12e0634385838d74c5b7a550522aaef
Joseph <coddow5@gmail.com>
2023-09-27 20:07:28 -0400

    Fixed the containers to only show the proper time bracket

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,39 @@
+// Get the current date and format it as desired (e.g., "MMMM D, YYYY")
+const currentDate = dayjs().format("MMMM D, YYYY");
 
+// Update the text content of the currentDay element
+document.getElementById("currentDay").textContent = currentDate;
+// Get the current hour (in 24-hour format)
+const currentHour = dayjs().hour();
 
-$(document).ready(function () {
-  // Display the current day at the top of the calendar
-  var currentDayEl = $("#currentDay");
-  currentDayEl.text(dayjs().format("dddd, MMMM D, YYYY"));
+// Loop through each time block
+document.querySelectorAll(".time-block").forEach((timeBlock) => {
+  const blockHour = parseInt(timeBlock.id.split("-")[1]);
 
-  // Generate time blocks for standard business hours
-  var container = $(".container-fluid");
-  var currentTime = dayjs().hour();
-
-  for (var hour = 9; hour <= 17; hour++) {
-    var timeBlockEl = $("<div>")
-      .addClass("row time-block")
-      .attr("id", "hour-" + hour);
-
-    var hourEl = $("<div>")
-      .addClass("col-2 col-md-1 hour text-center py-3")
-      .text(dayjs({ hour }).format("hA"));
-
-    var descriptionEl = $("<textarea>")
-      .addClass("col-8 col-md-10 description")
-      .attr("rows", "3");
-
-    var saveBtnEl = $("<button>")
-      .addClass("btn saveBtn col-2 col-md-1")
-      .attr("aria-label", "save")
-      .html('<i class="fas fa-save" aria-hidden="true"></i>');
-
-    timeBlockEl.append(hourEl, descriptionEl, saveBtnEl);
-    container.append(timeBlockEl);
-
-    // Color code time blocks based on the current time
-    if (hour < currentTime) {
-      timeBlockEl.addClass("past");
-    } else if (hour === currentTime) {
-      timeBlockEl.addClass("present");
-    } else {
-      timeBlockEl.addClass("future");
-    }
-  }
-
-  // Event listener for save buttons
-  $(".saveBtn").on("click", function () {
-    var eventText = $(this).siblings(".description").val();
-    var timeBlockId = $(this).parent().attr("id");
-    localStorage.setItem(timeBlockId, eventText);
-  });
-
-  // Load saved events from local storage
-  for (var hour = 9; hour <= 17; hour++) {
-    var savedEvent = localStorage.getItem("hour-" + hour);
-    if (savedEvent) {
-      $("#hour-" + hour + " .description").val(savedEvent);
-    }
+  if (blockHour < currentHour) {
+    timeBlock.classList.add("past");
+  } else if (blockHour == currentHour) {
+    timeBlock.classList.add("present");
+  } else {
+    timeBlock.classList.add("future");
   }
 });
+// Add event listeners to the save buttons
+document.querySelectorAll(".saveBtn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const timeBlockId = this.closest(".time-block").id;
+    const eventText = this.parentElement.querySelector(".description").value;
+
+    // Save the event to local storage using the time block's id as the key
+    localStorage.setItem(timeBlockId, eventText);
+  });
+});
+// Load saved events from local storage and populate the textareas
+for (let i = 9; i <= 17; i++) {
+  const timeBlockId = `hour-${i}`;
+  const eventText = localStorage.getItem(timeBlockId);
+
+  if (eventText) {
+    document.getElementById(timeBlockId).querySelector(".description").value = eventText;
+  }
+}
